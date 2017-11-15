771edfabde675829d326052a1a0cab9b52cac80d
SMAli <32557358+SMAliKSS@users.noreply.github.com>
2017-11-15 16:23:22 +0330

    App tracker application...
    App tracker application...
    not fully complete yet

---
+++ AppTracker/app/src/main/java/com/SMAli/apptracker/helper/FreemiumHelper.java
@@ -0,0 +1,11 @@
+package com.SMAli.apptracker.helper;
+
+import android.content.Context;
+
+public class FreemiumHelper {
+
+	public static boolean isAppTrackerPremiumInstalled(Context context) {
+		// App Tracker Premium has been deprecated
+		return true;
+	}
+}
