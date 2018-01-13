f4f6c8b71485bdc0a657e254fdc37f4d71df7c5f
SMAli <32557358+SMAliKSS@users.noreply.github.com>
2018-01-13 05:55:31 +0330

    Vianbo
    vianbo.com WebView application

---
+++ Vianbo/Vianbo/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace Vianbo
+{
+    [Activity(Label = "Vianbo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        WebView webView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.SetWebViewClient(new WebViewClient());
+            webView.Settings.JavaScriptEnabled = true;
+            webView.LoadUrl("http://polimi.vianbo.com");
+        }
+
+        public override void OnBackPressed()
+        {
+            webView.GoBack();
+        }
+    }
+}
+
